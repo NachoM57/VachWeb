78e65bf7bad2ddc1e996cb971988c1a014745e2e
NachoM57 <ignaciomaiorino50@gmail.com>
2024-01-18 21:00:09 -0300

    agregando efectos AOS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Code Web';
+
+
+  ngOnInit() {
+    AOS.init()
+    window.addEventListener('load', AOS.refresh)
+  }
 }
