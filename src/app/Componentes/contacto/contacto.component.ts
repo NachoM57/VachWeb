279c93eed6fde3e6d73db55fe92c62783ff6d539
NachoM57 <ignaciomaiorino50@gmail.com>
2024-04-13 19:12:11 -0300

    formulario de contacto funcional

--- src/app/Componentes/contacto/contacto.component.ts
+++ src/app/Componentes/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,9 +7,25 @@ import { Router } from '@angular/router';
   templateUrl: './contacto.component.html',
   styleUrls: ['./contacto.component.css']
 })
+
+
 export class ContactoComponent {
+  public sendContainer = false;
 
-  constructor(public router: Router) { }
+  constructor(private router: Router) { }
 
-
+  public sendEmail(e: Event): void {
+    e.preventDefault();
+    emailjs.sendForm('service_cvns2ne', 'template_zapi20g', e.target as HTMLFormElement, 'owTDrexC1cLMWOgjw')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+        this.sendContainer = true;
+        setTimeout(() => {
+          this.sendContainer = false;
+          this.router.navigate(['/inicio']);
+        }, 3000);
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
 }
